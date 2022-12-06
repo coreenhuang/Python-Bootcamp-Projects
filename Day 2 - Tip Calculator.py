0f5376acd08bf016d523812817ba8d6b9101be30
Coreen Huang <coreen.huang@gmail.com>
2022-12-06 00:21:14 -0500

    refactor rounding for bill per person

--- Day 2 - Tip Calculator.py
+++ Day 2 - Tip Calculator.py
@@ -10,6 +10,6 @@ multiplier = int(percentage_tip) / 100 + 1
 
 number_of_people = int(input("How many people to split the bill? "))
 
-bill_per_person = "{:.2f}".format(round( total_bill * multiplier / number_of_people, 2))
+bill_per_person = total_bill * multiplier / number_of_people
 
-print(f"Each person should pay: ${bill_per_person}")
+print(f"Each person should pay: ${bill_per_person:.2f}")
