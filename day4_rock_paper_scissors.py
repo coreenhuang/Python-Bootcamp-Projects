b7066a12329e97a9525c2fb82d9fabbc41a34910
Coreen Huang <coreen.huang@gmail.com>
2023-04-03 14:09:39 -0400

    update day 4 project, rock paper scissors

---
+++ day4_rock_paper_scissors.py
@@ -0,0 +1,37 @@
+import random
+
+# Rock, Paper, and Scissors
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+my_choice = random.randint(0,2)
+
+computer_choice = random.randint(0,2)
+
+print(my_choice)
+print(computer_choice)
