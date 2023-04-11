297d58e656fcb1c5e75e93da80a0e0953709397b
Coreen Huang <coreen.huang@gmail.com>
2023-04-11 18:14:29 -0400

    add day 6 practice exercise for reeborgs world

---
+++ practice_exercises/day6/day6_reeborgs_world.py
@@ -0,0 +1,19 @@
+# Move in a square
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+
+# Move over hurdles
+
