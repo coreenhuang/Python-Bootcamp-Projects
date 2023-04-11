49c4efc54d5945a37c1006b51bf7987b4c5433e0
Coreen Huang <coreen.huang@gmail.com>
2023-04-11 18:29:10 -0400

    add move over hurdle code to day 6 reeborgs world practice exercise

--- practice_exercises/day6/day6_reeborgs_world.py
+++ practice_exercises/day6/day6_reeborgs_world.py
@@ -17,3 +17,20 @@
 
 # Move over hurdles
 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def move_over_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for one_move in range(6):
+#     move_over_hurdle()
