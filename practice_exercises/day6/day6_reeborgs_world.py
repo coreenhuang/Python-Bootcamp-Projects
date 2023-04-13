b2f3affa60e44678793afac56ac45fbef6298669
Coreen Huang <coreen.huang@gmail.com>
2023-04-13 12:56:41 -0400

    add while loop version to reeborgs world hurdle exercise

--- practice_exercises/day6/day6_reeborgs_world.py
+++ practice_exercises/day6/day6_reeborgs_world.py
@@ -15,7 +15,7 @@
 # turn_right()
 # move()
 
-# Move over hurdles-----------
+# Move over hurdles using a for loop -----------
 
 # def turn_right():
 #     turn_left()
@@ -34,3 +34,27 @@
     
 # for one_move in range(6):
 #     move_over_hurdle()
+
+# Move over hurdles using a while loop -----------
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def move_over_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# number_of_hurdles = 6
+
+# while number_of_hurdles > 0:
+#     move_over_hurdle()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
