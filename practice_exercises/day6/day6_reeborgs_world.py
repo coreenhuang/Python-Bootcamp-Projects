1297356eb4bf3fae1972f1d79fee2dad5961161a
Coreen Huang <coreen.huang@gmail.com>
2023-04-16 17:43:36 -0400

    add while loop for random goal exercise in reeborgs world

--- practice_exercises/day6/day6_reeborgs_world.py
+++ practice_exercises/day6/day6_reeborgs_world.py
@@ -58,3 +58,28 @@
 #     move_over_hurdle()
 #     number_of_hurdles -= 1
 #     print(number_of_hurdles)
+
+# Move over hurdles using a while loop but goal is random-----------
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def move_over_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# while at_goal() == False:
+#     move_over_hurdle()
+
+# OR
+
+# while not at_goal():
+#     move_over_hurdle()
