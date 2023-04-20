04825a83fd741b4c74ce479a41df92559174f24e
Coreen Huang <coreen.huang@gmail.com>
2023-04-19 23:12:06 -0400

    comment out code for the time being to avoid error messages

--- day6_escaping_maze.py
+++ day6_escaping_maze.py
@@ -1,18 +1,18 @@
 # Maze Challenge in Reeborg's world
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
         
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
 
 # Intermediate Debugging Challenge against infinite loop scenarios
 # Come back after day 15
--- practice_exercises/day6/day6_reeborgs_world.py
+++ practice_exercises/day6/day6_reeborgs_world.py
@@ -132,19 +132,19 @@
 
 # Maze Challenge -----------------------------
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
         
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
 
 # Intermediate Debugging Challenge against infinite loop scenarios
 # Come back after day 15
