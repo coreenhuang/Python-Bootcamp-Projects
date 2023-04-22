945705ea12a60300e1074786f3826e9fcb71e58c
Coreen Huang <coreen.huang@gmail.com>
2023-04-21 20:45:47 -0400

    select random word from word list

--- practice_exercises/day7/challenge1.py
+++ practice_exercises/day7/challenge1.py
@@ -1,8 +1,15 @@
 # Picking a random word and checking answers
+import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = random.choice(word_list)
+
+word_split = chosen_word.split()
+
+print(word_split)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
