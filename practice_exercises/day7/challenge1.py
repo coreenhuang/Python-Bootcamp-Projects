762c3ac65f21c6277e0883a2e13b6178433c7ff0
Coreen Huang <coreen.huang@gmail.com>
2023-04-21 21:48:30 -0400

    ensure guess input is lower case

--- practice_exercises/day7/challenge1.py
+++ practice_exercises/day7/challenge1.py
@@ -13,7 +13,7 @@ word_split = list(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ")
+guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
