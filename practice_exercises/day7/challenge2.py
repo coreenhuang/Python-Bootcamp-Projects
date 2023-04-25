ebf16bda0bc4727a9dac7cb30b3c6cc8047cc2e0
Coreen Huang <coreen.huang@gmail.com>
2023-04-24 21:39:49 -0400

    include alternative for loop solution

--- practice_exercises/day7/challenge2.py
+++ practice_exercises/day7/challenge2.py
@@ -39,6 +39,15 @@ for letter in chosen_word:
 
     index += 1
 
+# ALTERNATIVE FOR LOOP
+
+# for position in range(word_length):
+    
+#     letter = chosen_word[position]
+
+#     if letter == guess:
+#         display[position] = letter
+
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
