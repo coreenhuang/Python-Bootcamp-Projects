38fd2d3d6266ad4afd07f103863db770b87a1d99
Coreen Huang <coreen.huang@gmail.com>
2023-05-03 18:34:05 -0400

    delete initial word list used for testing

--- practice_exercises/day7/challenge5.py
+++ practice_exercises/day7/challenge5.py
@@ -7,7 +7,7 @@ from hangman_art import *
 from hangman_words import word_list
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
