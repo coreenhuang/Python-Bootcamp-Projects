290898e36b84c41247e5f505c6ae8042950fb2f8
Coreen Huang <coreen.huang@gmail.com>
2023-05-07 10:38:19 -0400

    insert review notes and code introducing functions with inputs

--- practice_exercises/day8/review.py
+++ practice_exercises/day8/review.py
@@ -2,3 +2,18 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+
+def greet():
+    print('Hello')
+    print('to')
+    print('you!')
+
+greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you, {name}?")
+
+greet_with_name("Bobby")
