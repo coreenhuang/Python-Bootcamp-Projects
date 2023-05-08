e6eacfe931bb9a364fc79ea3d10b81849f23feab
Coreen Huang <coreen.huang@gmail.com>
2023-05-08 09:39:15 -0400

    add function with more than one input

--- practice_exercises/day8/review.py
+++ practice_exercises/day8/review.py
@@ -3,17 +3,25 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
-def greet():
-    print('Hello')
-    print('to')
-    print('you!')
+# def greet():
+#     print('Hello')
+#     print('to')
+#     print('you!')
 
-greet()
+# greet()
 
 # Function that allows for input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How are you, {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you, {name}?")
 
-greet_with_name("Bobby")
+# greet_with_name("Bobby")
+
+# Functions with more than one input
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Jimmy Neutron", "LaLaLand")
