0acb20c6c302696b394113fac376a311e76da8ad
Coreen Huang <coreen.huang@gmail.com>
2023-05-11 22:12:27 -0400

    complete prime number exercise

--- practice_exercises/day8/prime_number_checker.py
+++ practice_exercises/day8/prime_number_checker.py
@@ -1,4 +1,14 @@
+# Prime Number Checker
 
+def prime_checker(number):
+    count = 0
+    for x in range(2,number):
+        if number % x == 0:
+            count += 1
+    if count > 0:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
