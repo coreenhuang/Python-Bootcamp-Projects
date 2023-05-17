241b1fd36e8e1a19c357e5beb10acce99c69c314
Coreen Huang <coreen.huang@gmail.com>
2023-05-16 22:44:19 -0400

    complete cipher 3, reorganizing code

--- practice_exercises/day8/caesar_cipher_3.py
+++ practice_exercises/day8/caesar_cipher_3.py
@@ -8,25 +8,43 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
+
+def caesar(input_text, shift_amount, direction_value):
+  output_text = ""
+
+  for letter in input_text:
+
+    position = alphabet.index(letter)
+
+    if direction_value == "encode":
+      new_position = position + shift_amount
+    elif direction_value == "decode":
+      new_position = position - shift_amount
+
+    output_text += alphabet[new_position]
+
+  print(f"The output text is {output_text}")
 
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(input_text=text, shift_amount=shift, direction_value=direction)
