a6c07a8ee5d7dfd235893e0bd4d9065170b6fada
Coreen Huang <coreen.huang@gmail.com>
2023-05-28 12:56:18 -0400

    commit some code logic into blind auction

--- practice_exercises/day9/blind_auction.py
+++ practice_exercises/day9/blind_auction.py
@@ -1,2 +1,17 @@
 from replit import clear
+from art import logo
 #HINT: You can call clear() to clear the output in the console.
+
+print(logo)
+bidders = {}
+keep_bidding = True
+
+while keep_bidding:
+  bidder = input("What is your name? ")
+  bid = int(input("How much would you like to bid? $"))
+  
+  bidders[bidder] = bid
+  
+  print(bidders)
+  
+  more_bidders = input("Is there anyone else who would like to bid? Type 'yes' or 'no'.")
