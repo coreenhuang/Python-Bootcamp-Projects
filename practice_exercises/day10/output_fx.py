ca53eb07bf2228299a01c0ce5f330c6a00406bba
Coreen Huang <coreen.huang@gmail.com>
2023-05-29 15:32:33 -0400

    add in a return into the function with output

--- practice_exercises/day10/output_fx.py
+++ practice_exercises/day10/output_fx.py
@@ -2,5 +2,6 @@
 
 def format_name(f_name, l_name):
     print(f_name.capitalize() + ' ' + l_name.capitalize())
+    return f_name.capitalize() + ' ' + l_name.capitalize()
 
 format_name('COREEN', 'HUANG')
