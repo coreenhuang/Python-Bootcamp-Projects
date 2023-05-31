e9b0ddea01ee335cdae3f8d0ff69d14b671a420e
Coreen Huang <coreen.huang@gmail.com>
2023-05-31 19:42:15 -0400

    input integers num1 and num2, and print each operation

--- practice_exercises/day10/calculator.py
+++ practice_exercises/day10/calculator.py
@@ -22,3 +22,9 @@ operations = {
     "*": multiply,
     "/": divide
 }
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for operation in operations:
+    print(operation)
