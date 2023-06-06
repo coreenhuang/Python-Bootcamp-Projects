3ade5a79c0b9404e3ed611cb8f9bbece70663f76
Coreen Huang <coreen.huang@gmail.com>
2023-06-06 19:03:38 -0400

    add blackjack code for first round of cards being distributed

--- day11_blackjack.py
+++ day11_blackjack.py
@@ -11,7 +11,23 @@ logo = """
 
 start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
+import random
+
+card_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 if start_game == 'y':
     print(logo)
+
+    #First round
+    first_card = random.choice(card_options)
+    second_card = random.choice(card_options)
+    user_total = first_card + second_card
+
+    computer_card = random.choice(card_options)
+
+    print(f"    Your cards: [{first_card}, {second_card}], current score: {user_total}")
+    print(f"    Computer's first card: {computer_card}")
+
+
 else:
     exit()
