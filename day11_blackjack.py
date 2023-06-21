d668851cb44797f79152c9166e1117dcb42f2c5d
Coreen Huang <coreen.huang@gmail.com>
2023-06-21 19:00:20 -0400

    create deal card function

--- day11_blackjack.py
+++ day11_blackjack.py
@@ -121,7 +121,12 @@ logo = """
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+    return random.choice(cards)
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
