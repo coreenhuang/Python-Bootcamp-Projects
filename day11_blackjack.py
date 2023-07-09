6f4a862dd59a20b908726b3cf57c9572e0a0cd5f
Coreen Huang <coreen.huang@gmail.com>
2023-07-09 19:56:37 -0400

    check for blackjack and cards length is 2

--- day11_blackjack.py
+++ day11_blackjack.py
@@ -65,10 +65,13 @@ for deal in range(2):
 #and returns the score. 
 #Look up the sum() function to help you do this.
 
-def calculate_score(list):
-    return sum(list)
+def calculate_score(cards):
+    #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
 
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    return sum(cards)
+
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 
