db175ff4b5b2404638eccc45552fce53db38f7e8
coreenhuang <coreen.huang@gmail.com>
2023-09-03 21:36:01 -0400

    reformat code and correct errors

--- day12_guess_the_number.py
+++ day12_guess_the_number.py
@@ -9,20 +9,21 @@ logo = """
                                                                                            
 """
 
-#Function to svaluate guess with target answer
+#Function to evaluate guess with target answer
 def evaluate_guess(guess, target, turns):
-   """compare guess with target, returns remaining number of attempts"""
+   """Compare guessed number with target number. Returns remaining number of lives."""
    if guess == target:
       print(f"You win! The answer is {target}.")
    elif guess > target:
-      print(f"Your guess is too high. Guess again.")
+      print(f"Your guess is too high.")
       return turns - 1
    else:
-      print(f"Your guess is too low. Guess again.")
+      print(f"Your guess is too low.")
       return turns - 1
 
 #Function to return number of lives depending on difficulty selection
 def select_difficulty():
+   """Return number of lives depending on difficulty level."""
    difficulty = input("Choose a diffculty. Type 'easy' or 'hard': ")
    if difficulty == "easy":
       return 10
@@ -45,13 +46,19 @@ def game_on():
 
    while guess_input != target_number:
 
+      print(f"You have {available_lives} attempts remaining to guess the number.")
+
       #User to input a guess
       guess_input = int(input("Please make a guess: "))
 
+      #Evaluate guess with target answer, reduce 1 life if incorrect
+      available_lives = evaluate_guess(guess_input, target_number, available_lives)
 
-
-
-
-
+      #End game if no more lives available
+      if available_lives == 0:
+         print("You have no more attempts left. You lose.")
+         return
+      elif guess_input != target_number:
+         print("Please guess another number.")
 
 game_on()
