9211cb56d72532e47d1aebcfabf333217946fe30
coreenhuang <coreen.huang@gmail.com>
2023-09-08 18:31:38 -0400

    access values of data dictionary, print to check

--- day14/day14_higher_lower.py
+++ day14/day14_higher_lower.py
@@ -1,6 +1,10 @@
 from art import *
-# from data import data
+from data import data
 
 print(logo)
 print(vs)
 
+print(data[0]['name'])
+print(data[0]['follower_count'])
+print(data[0]['description'])
+print(data[0]['country'])
