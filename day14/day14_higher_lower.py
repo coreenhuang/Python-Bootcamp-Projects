ba5c8618f688c0ffd022a27636de4029a39602bb
coreenhuang <coreen.huang@gmail.com>
2023-09-09 10:48:36 -0400

    import random randint, generate random index number

--- day14/day14_higher_lower.py
+++ day14/day14_higher_lower.py
@@ -1,8 +1,11 @@
 from art import *
 from data import data
+from random import randint
 
 print(logo)
 print(vs)
+
+print(randint(0,len(data) - 1))
 
 print(data[0]['name'])
 print(data[0]['follower_count'])
